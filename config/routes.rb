293a0933c8a28ab725e35c9a27b70b2a18fef422
Ico Chen <icelandsoundsneat@outlook.com>
2018-04-03 16:20:14 +0800

    wiki done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,54 @@
 Rails.application.routes.draw do
+  # get 'wiki/list'
+
+  # get 'wiki/show'
+
+  # post 'wiki/edit'
+
+  # post 'wiki/create'
+
+  # patch 'wiki/update'
+
+  # delete 'wiki/destroy'
+
+  resources :wiki
+
+#     def index
+#     @articles = Article.all
+#   end
+
+#   def show
+#     @article = Article.find(params[:id])
+#   end
+
+#   def new
+#     @article = Article.new
+#   end
+
+#   def create
+#     @article = Article.new(article_params)
+#     @article.save
+#     redirect_to wiki_index_path(@article)
+#   end
+
+#   def edit
+#     @article = Article.find(params[:id])
+#   end
+
+#   def update
+#     @article = Article.find(params[:id])
+#     @article.update(article_params)
+#   end
+
+#   def destroy
+#   end
+
+#   private
+
+#   def article_params
+#     params.require(:article).permit(:title, :content)
+#   end
+# end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
